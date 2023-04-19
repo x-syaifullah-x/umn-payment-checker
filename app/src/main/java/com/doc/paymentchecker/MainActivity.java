3f46a7bbb49f88bfb8024506e176017669280c2f
x-syaifullah-x <x.syaifullah.x@gmail.com>
2023-04-19 12:02:53 +0700

    displayed username

--- app/src/main/java/com/doc/paymentchecker/MainActivity.java
+++ app/src/main/java/com/doc/paymentchecker/MainActivity.java
@@ -2,8 +2,11 @@ package com.doc.paymentchecker;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,28 +24,37 @@ import com.android.volley.toolbox.Volley;
 import com.applovin.mediation.ads.MaxAdView;
 import com.applovin.sdk.AppLovinSdk;
 import com.applovin.sdk.AppLovinSdkSettings;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
+import java.util.Locale;
 import java.util.Objects;
 import java.util.TimeZone;
 
 public class MainActivity extends FragmentActivity {
-    private Button connect;
-    private EditText usernameed;
-    private EditText passworded;
-    private String username;
-    private String password;
-    private TextView Expiry;
-    private android.widget.FrameLayout login;
-    private android.widget.FrameLayout qr;
-    private SharedPreferences myPrefs;
+
+    private static final String SHARED_PREFERENCES_NAME = "myPrefs";
+
+    private static final String SHARED_PREFERENCES_KEY_USERNAME = "usernam";
+
+    private static final String SHARED_PREFERENCES_KEY_PASSWORD = "passwor";
+
+    private EditText etUserName;
+
+    private EditText etPassword;
+
+    private TextView tvExpiry;
+
+    private TextView tvUserName;
+
+    private android.widget.FrameLayout vgLogin;
+
+    private android.widget.FrameLayout vgQR;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -51,24 +63,28 @@ public class MainActivity extends FragmentActivity {
 
         loadAd(findViewById(R.id.adView));
 
-        usernameed = findViewById(R.id.username_textbox);
-        passworded = findViewById(R.id.password_textbox);
-        connect = findViewById(R.id.login_button);
-        Expiry = findViewById(R.id.expiry_text);
-        login = findViewById(R.id.login_layout);
-        qr = findViewById(R.id.qr_code_layout);
-        myPrefs = getSharedPreferences("myPrefs", MODE_PRIVATE);
-        if (!(Objects.requireNonNull(myPrefs.getString("usernam", "")).isEmpty() && Objects.requireNonNull(myPrefs.getString("passwor", "")).isEmpty())) {
-            chkexpiry(myPrefs.getString("usernam", ""), myPrefs.getString("passwor", ""));
+        tvUserName = findViewById(R.id.username);
+        etUserName = findViewById(R.id.username_textbox);
+        etPassword = findViewById(R.id.password_textbox);
+        tvExpiry = findViewById(R.id.expiry_text);
+        vgQR = findViewById(R.id.qr_code_layout);
+        vgLogin = findViewById(R.id.login_layout);
+
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE);
+        String username = sharedPreferences.getString(SHARED_PREFERENCES_KEY_USERNAME, "");
+        String password = sharedPreferences.getString(SHARED_PREFERENCES_KEY_PASSWORD, "");
+        if (!username.isEmpty() && !password.isEmpty()) {
+            chkexpiry(username, password);
         } else {
-            login.setVisibility(View.VISIBLE);
+            vgLogin.setVisibility(View.VISIBLE);
         }
-        connect.setOnClickListener(view -> {
-            username = String.valueOf(usernameed.getText());
-            password = String.valueOf(passworded.getText());
-            if (!(username.isEmpty() || password.isEmpty())) {
-                chkexpiry(username, password);
-            }
+
+        findViewById(R.id.login_button).setOnClickListener(view -> {
+            String usernameInput = etUserName.getText().toString();
+            String passwordInput = etPassword.getText().toString();
+            if (usernameInput.isEmpty()) return;
+            if (passwordInput.isEmpty()) return;
+            chkexpiry(usernameInput, passwordInput);
         });
     }
 
@@ -85,46 +101,57 @@ public class MainActivity extends FragmentActivity {
 
     private void chkexpiry(String user, String pass) {
         RequestQueue MyRequestQueue = Volley.newRequestQueue(this);
-        String url = "http://line.my-tv.cc/player_api.php?username=" + user.trim() + "&password=" + pass.trim();
+        Uri uri = new Uri.Builder()
+                .scheme("http")
+                .authority("line.my-tv.cc")
+                .path("player_api.php")
+                .appendQueryParameter("username", user.trim())
+                .appendQueryParameter("password", pass.trim())
+                .build();
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, uri.toString(), response -> {
+            try {
+                JSONObject jsonObject = new JSONObject(response);
+                JSONObject userInfo = jsonObject.getJSONObject("user_info");
+                String expDate = userInfo.getString("exp_date");
+                long expDateLong = Long.parseLong(expDate);
+                Date date = new Date(expDateLong * 1000L);
+                //SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy", Locale.getDefault());
+                sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+                String expDateFormatted = sdf.format(date);
+                System.out.println("Expiration date: " + expDateFormatted);
+                String userName = userInfo.getString("username");
+                tvUserName.setText("USERNAME: " + userName);
+                tvExpiry.setText("EXPIRATION: " + expDateFormatted);
+                vgLogin.setVisibility(View.GONE);
+                vgQR.setVisibility(View.VISIBLE);
+                SharedPreferences.Editor edit = getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE).edit();
+                edit.putString(SHARED_PREFERENCES_KEY_USERNAME, user);
+                edit.putString(SHARED_PREFERENCES_KEY_PASSWORD, pass);
+                edit.apply();
 
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        Log.e("firees", response);
-                        if (response.contains("exp_date")) {
-                            ObjectMapper objectMapper = new ObjectMapper();
-                            try {
-                                Map<String, Object> jsonMap = objectMapper.readValue(response, new TypeReference<Map<String, Object>>() {
-                                });
-                                String expDate = (String) ((Map<String, Object>) jsonMap.get("user_info")).get("exp_date");
-                                long expDateLong = Long.parseLong(expDate);
-                                Date date = new Date(expDateLong * 1000L);
-                                //SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                                SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
-                                sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
-                                String expDateFormatted = sdf.format(date);
-                                System.out.println("Expiration date: " + expDateFormatted);
-                                Expiry.setText("EXPIRATION: " + expDateFormatted);
-                                login.setVisibility(View.GONE);
-                                qr.setVisibility(View.VISIBLE);
-                                SharedPreferences.Editor e1 = myPrefs.edit();
-                                e1.putString("usernam", user); // add or overwrite someValue
-                                e1.putString("passwor", pass);
-                                e1.apply();
-                                //Toast.makeText(MainActivity.this,"Expiration date: " + expDateFormatted,Toast.LENGTH_LONG).show();
-
-                            } catch (JsonProcessingException e) {
-                                e.printStackTrace();
-                            }
-
-                        } else {
-
-                            Toast.makeText(MainActivity.this, "Make sure the User & Password is correct", Toast.LENGTH_LONG).show();
-                        }
-                        // Handle response
-                    }
-                }, new Response.ErrorListener() {
+                StringBuilder sb = new StringBuilder();
+                Resources r = getResources();
+                float screenWidth = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 400, r.getDisplayMetrics()));
+                String text = "Your UMN Q96 Mate has been updated. Thanks for your support";
+                sb.replace(0, sb.toString().length(), "");
+                sb.append(text);
+                Paint paint = new Paint();
+                TextView view = findViewById(R.id.message_board);
+                paint.setTextSize(view.getTextSize());
+                float textWidth = paint.measureText(sb.toString());
+//                                float screenWidth = getResources().getDisplayMetrics().widthPixels;
+                if (textWidth <= screenWidth) {
+                    int padding = (int) (screenWidth - textWidth) / 2;
+                    view.setPadding(padding, 0, padding, 0);
+                    sb.insert(0, "     ").append("     ");
+                }
+                view.setText(sb.toString());
+                view.setSelected(true);
+            } catch (JSONException e) {
+                Toast.makeText(MainActivity.this, "Make sure the User & Password is correct", Toast.LENGTH_LONG).show();
+            }
+        }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 Toast.makeText(MainActivity.this, "Error make sure values are correct", Toast.LENGTH_LONG).show();
